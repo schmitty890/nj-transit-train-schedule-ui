38c5d9a5c5057a79b135270b38a7965abf7186ed
Jason Schmitt <schmitty890@gmail.com>
2019-07-02 20:32:54 -0400

    adding react bootstrap to header

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
 import styles from "./Hero.module.css";
 
 const Hero = () => {
     return (
-        <div>hero</div>
+        <Container>
+        <Row>
+            <Col sm={8}>sm=8</Col>
+            <Col sm={4}>sm=4</Col>
+        </Row>
+        <Row>
+            <Col sm>sm=true</Col>
+            <Col sm>sm=true</Col>
+            <Col sm>sm=true</Col>
+        </Row>
+        </Container>
     )
 }
 
