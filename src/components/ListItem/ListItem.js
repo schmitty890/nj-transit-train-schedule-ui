be2180574f7649f309ff60201ea84d5180dcbccc
Jason Schmitt <schmitty890@gmail.com>
2019-07-05 01:11:27 -0400

    adding n/a status

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -4,18 +4,51 @@ import styles from "./ListItem.module.css";
 
 const ListItem = (props) => {
     console.log('the props');
-    console.log(props);
+    console.log(props.status);
+    let status = '';
+    let destination = '';
+    if(props.status === '') {
+        console.log('status is empty');
+        status = 'n/a';
+    } else {
+        status = props.status;
+    }
+
+    if(props.destination === '') {
+        console.log('destination is empty');
+        destination = 'n/a';
+    } else {
+        destination = props.destination;
+    }
     return (            
         <Card>
             <Card.Header>
             <Accordion.Toggle as={Button} variant="link" eventKey={props.keyid}>
                 <section className={styles.trainWrapper}>
-                    <div>departure: {props.departure}</div>
-                    <div>destination: {props.destination}</div>
-                    <div>track #: {props.track}</div>
-                    <div>line: {props.line}</div>
-                    <div>status: {props.status}</div>
-                    <div>train #: {props.trainNumber}</div>
+                    <div className={styles.trainInfo}>
+                        <div>departure</div>
+                        <div>{props.departure}</div>
+                    </div>
+                    <div className={styles.trainInfo}>
+                        <div>destination</div>
+                        <div>{destination}</div>
+                    </div>
+                    <div className={styles.trainInfo}>
+                        <div>track #</div>
+                        <div>{props.track}</div>
+                    </div>
+                    <div className={styles.trainInfo}>
+                        <div>line</div>
+                        <div>{props.line}</div>
+                    </div>
+                    <div className={styles.trainInfo}>
+                        <div>status</div>
+                        <div>{status}</div>
+                    </div>
+                    <div className={styles.trainInfo}>
+                        <div>trainNumber</div>
+                        <div>{props.trainNumber}</div>
+                    </div>
                 </section>
             </Accordion.Toggle>
             </Card.Header>
