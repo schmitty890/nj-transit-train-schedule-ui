00a1d0aace79401419232efa91c82076cceb6f56
Jason Schmitt <schmitty890@gmail.com>
2019-07-10 22:52:21 -0400

    adding weather icons stylesheet and utility helper to map icon to icon number

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Row, Col, Container, Button, Accordion, Card, Dropdown } from 'react-bootstrap';
 import styles from "./CurrentWeather.module.css";
+import weatherIconAPI from "../../utils/weatherIcons/weatherIcons";
 
 const CurrentWeather = (props) => {
     console.log(props);
+    const icon = weatherIconAPI.getIcon(props.currentWeather[9].icon);
     return (
         <Container>
             <Row>
@@ -13,7 +15,7 @@ const CurrentWeather = (props) => {
             <Row>
                 <Col sm>wind speed: {props.currentWeather[3].windSpeed}</Col>
                 <Col sm>humidity: {props.currentWeather[4].humidity}</Col>
-                <Col sm>icon: {props.currentWeather[9].icon}</Col>
+                <Col sm>icon: {icon}</Col>
             </Row>
         </Container>
     )
---
+++ src/utils/weatherIcons/weatherIcons.js
@@ -0,0 +1,42 @@
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+
+export default {
+    getIcon: function(icon) {
+        console.log(icon);
+        switch(icon) {
+            case '01d': // clear day
+                return ReactHtmlParser('<i class="wi wi-day-sunny"></i>');
+            case '01n': // clear night
+                return ReactHtmlParser('<i class="wi wi-night-clear"></i>');
+            case '02d': // cloudy day
+            case '03d':
+            case '04d':
+                return ReactHtmlParser('<i class="wi wi-day-cloudy"></i>');
+            case '02n':// clouds night
+            case '03n':
+            case '04n':
+                return ReactHtmlParser('<i class="wi wi-night-alt-cloudy"></i>');
+            case '09d': // rain day
+            case '10d':
+                return ReactHtmlParser('<i class="wi wi-day-rain"></i>');
+            case '09n': // rain night
+            case '10n':
+                return ReactHtmlParser('<i class="wi wi-night-alt-rain"></i>');
+            case '11d': // thunderstorm day
+                return ReactHtmlParser('<i class="wi wi-day-thunderstorm"></i>');
+            case '11n': // thunderstorm night
+                return ReactHtmlParser('<i class="wi wi-night-alt-thunderstorm"></i>');
+            case '13d': // snow day
+                return ReactHtmlParser('<i class="wi wi-day-snow"></i>');
+            case '13n': // snow night
+                return ReactHtmlParser('<i class="wi wi-night-snow"></i>');
+            case '50d': // mist day
+                return ReactHtmlParser('<i class="wi wi-day-fog"></i>');
+            case '50n': // mist night
+                return ReactHtmlParser('<i class="wi wi-night-fog"></i>');
+
+            default: // default N/A icon shown
+                return ReactHtmlParser('<i class="wi wi-na"></i>');
+          }
+    }
+};
