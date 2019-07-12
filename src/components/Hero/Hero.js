27ddc7e4a420c51db6aef0abf992c6afbb39e6c5
Jason Schmitt <schmitty890@gmail.com>
2019-07-11 23:24:16 -0400

    adding logo images and some styling

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Row, Col, Container, Button, Accordion, Card, Dropdown } from 'react-bootstrap';
 import styles from "./Hero.module.css";
 import CurrentWeather from '../CurrentWeather/CurrentWeather';
+import logo from '../../images/logo.png';
 
 const Hero = (props) => {
     // console.log(props);
@@ -11,7 +12,7 @@ const Hero = (props) => {
                 currentWeather={props.currentWeather} />
             <Row>
                 <Col sm={12} className={styles.hero}>
-                    <img className={styles.heroImage} src="//via.placeholder.com/250x100" data-src="//via.placeholder.com/250x100" />
+                    <img className={styles.heroImage} src={logo} />
                     <div className={styles.heroTitle}>Current Train Schedule: {props.currentStation}</div>
                     <Dropdown>
                         <Dropdown.Toggle variant="success" id="dropdown-basic">
